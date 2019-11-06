583e928b9d19ca3f1014942199001dc59ece007f
Francisco Alejandro Hoyos Rojas <fralejanro@gmail.com>
2019-11-06 10:45:48 -0500

    Actualización método eliminar libro;

--- ambiente/semillero-hbt/semillero-padre/semillero-web/src/main/angular/src/app/semillero/componentes/gestionarLibro/gestionar-libro-component.ts
+++ ambiente/semillero-hbt/semillero-padre/semillero-web/src/main/angular/src/app/semillero/componentes/gestionarLibro/gestionar-libro-component.ts
@@ -13,12 +13,17 @@ import { Libro } from './libro.model';
     styleUrls: ['./gestionar-libro-component.css']
 })
 
-export class GestionarLibroComponent implements OnInit{
+export class GestionarLibroComponent implements OnInit {
 
     /**
      * Atributo que representa la lista de libros
      */
     public libros: Array<Libro>;
+
+    /**
+     * Atributo que representa si un libro se elimino o no
+     */
+    public validar: boolean;
 
     /**
      * Atributo que reprensenta un mensaje cuando se elimina un libro
@@ -29,14 +34,15 @@ export class GestionarLibroComponent implements OnInit{
     /**
      * Constructor del componente
      */
-    constructor(){
+    constructor() {
 
     }
 
-    ngOnInit(){
+    ngOnInit() {
         this.libros = new Array<Libro>();
         this.crearLibros(this.libros);
         this.mensaje = "";
+        this.validar = false;
 
     }
 
@@ -44,33 +50,35 @@ export class GestionarLibroComponent implements OnInit{
      * Método utilizado para crear y agregar libros a una lista de libros
      * @param libros lista donde se van a crear y agregar libros
      */
-    public crearLibros(libros: Array<Libro>): void{
-       let libro = new Libro(1,  "Rick y Morty", "Norma", 100, 10000, "Ryan Ridley", true,new Date("2013-01-01"),"Activo");
-       libros.push(libro);
-       libro = new Libro(2,  "Flash", "DC Comics", 200, 20000, "David Nutter", true,new Date("2014-02-01"),"Activo");
-       libros.push(libro);
-       libro = new Libro(3,  "Los espiritus de los muertos", "Planeta Comic", 300, 30000, "Edgar Allan Poe", true,new Date("2015-05-05"),"Inactivo");
-       libros.push(libro);
-       libro = new Libro(4,  "El misterio de Salem's Lo", "Libreria Nacional", 400, 40000, "Stephen King", true,new Date("2018-05-05"),"Activo");
-       libros.push(libro);
-       libro = new Libro(5,  "El gato negro", "Planeta Comic", 500, 30000, "Edgar Allan Poe", true,new Date("2010-01-01"),"Inactivo");
-       libros.push(libro);
+    public crearLibros(libros: Array<Libro>): void {
+        let libro = new Libro(1, "Rick y Morty", "Norma", 100, 10000, "Ryan Ridley", true, new Date("2013-01-01"), "Activo");
+        libros.push(libro);
+        libro = new Libro(2, "Flash", "DC Comics", 200, 20000, "David Nutter", true, new Date("2014-02-01"), "Activo");
+        libros.push(libro);
+        libro = new Libro(3, "Los espiritus de los muertos", "Planeta Comic", 300, 30000, "Edgar Allan Poe", true, new Date("2015-05-05"), "Inactivo");
+        libros.push(libro);
+        libro = new Libro(4, "El misterio de Salem's Lo", "Libreria Nacional", 400, 40000, "Stephen King", true, new Date("2018-05-05"), "Activo");
+        libros.push(libro);
+        libro = new Libro(5, "El gato negro", "Planeta Comic", 500, 30000, "Edgar Allan Poe", true, new Date("2010-01-01"), "Inactivo");
+        libros.push(libro);
     }
 
     /**
      * Método utilizado para eliminar un libro por id
      * @param id id del libro a eliminar
      */
-    public eliminarLibro(id: number): void{
+    public eliminarLibro(id: number): void {
         let libro: Libro;
         for (let index = 0; index < this.libros.length; index++) {
             libro = this.libros[index];
-           if(libro.id == id){
-            this.libros.splice(index,1);
-            this.mensaje = "El libro " + libro.nombre + "fue eliminado";
-            return;
-           }
+            if (libro.id == id) {
+                this.libros.splice(index, 1);
+                this.validar = true;
+                this.mensaje = "El libro " + libro.nombre + " fue eliminado";
+                return;
+            }
         }
+        this.validar = false;
         this.mensaje = "El libro con id 3 no existe";
     }
 }
